2e3570297e33fe6e27ee584f5bf814d4bfcb1dd2
Valeria Leticia Silva de Luna <valeria.luna@mkth.com.br>
2023-07-06 15:19:13 -0300

    feat: add remove function

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,35 +1,29 @@
-import { useState, useMemo, useEffect } from 'react'
+import { useState } from 'react'
 import { Container, Box, LinearProgress, SelectChangeEvent } from "@mui/material"
 import { Table } from "@/components/Table/Table"
 import { cells } from "@/components/Table/constants"
 import { useQuery } from "react-query"
 import { getPosts } from "@/services/users"
 import { Rows } from '@/types/general'
+import { useSnackbarStore } from '@/store/general'
 
 export const Posts = () => {
-  const { data, isLoading, isFetching } = useQuery({ queryKey: ['posts'], queryFn: getPosts})
-
   const [page, setPage] = useState(1)
   const [rowsPerPage, setRowsPerPage] = useState(5)
   const [rows, setRows] = useState<Rows[]>([]) 
-
-  //TODO ver qual opção fica melhor sobre esse state para as rows
-  const visibleRows = useMemo(
-    () =>
-    data?.data.slice(
-      page * rowsPerPage,
-      page * rowsPerPage + rowsPerPage,
-    ),
-    [data?.data, page, rowsPerPage]
-  )
-
-  /*useEffect(() => {
-    setRows(data?.data)
-    rows.slice(
-      page * rowsPerPage,
-      page * rowsPerPage + rowsPerPage
-    )
-  }, [rows, page, rowsPerPage, data?.data])*/
+  const handleAddMessage = useSnackbarStore(state => state.handleAddMessage)
+  
+  const { data, isLoading, isFetching } = useQuery({ 
+    queryKey: ['posts', {page: page, rowsPerPage: rowsPerPage}], 
+    queryFn: getPosts,
+    keepPreviousData: true,
+    onSuccess() {
+      const startIndex = (page - 1 ) * rowsPerPage
+      const endIndex = startIndex + rowsPerPage
+      const response = data?.data.slice(startIndex, endIndex)
+      setRows(response)
+    }
+  })
 
   const handlePageChange = (event: unknown, newPage: number) => {
     setPage(newPage)
@@ -41,7 +35,8 @@ export const Posts = () => {
 
   const handleRemoveRow = (id: string) => {
     //TODO fazer esse remove funcionar e adicionar o snaback
-    data?.data.filter((item: { id: string }) => item.id !== id) as Rows[]
+    setRows(rows => rows?.filter((row) => row.id.toString() !== id))
+    handleAddMessage('Removido com sucesso!', 'success')
     console.log('PASSOU AQUI@@@')
   }
   
@@ -53,7 +48,7 @@ export const Posts = () => {
         </Box>
       }
       <Table 
-        rows={visibleRows} 
+        rows={rows} 
         cells={cells} 
         loading={isLoading} 
         page={page}
